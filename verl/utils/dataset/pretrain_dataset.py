33942b5a80baedc8c13f82539810756a9a10f872
libufan <libufan@bytedance.com>
2026-02-10 13:59:01 +0800

    fix filter_overlong_prompts in MultiTurnSFTDataset and PretrainDataset

--- verl/utils/dataset/pretrain_dataset.py
+++ verl/utils/dataset/pretrain_dataset.py
@@ -121,7 +121,8 @@ class PretrainDataset(Dataset):
             dataframe = pd.read_parquet(parquet_file, dtype_backend="pyarrow")
             dataframes.append(dataframe)
         self.dataframe = pd.concat(dataframes)
-
+        self.dataframe = self.maybe_filter_out_long_prompts(self.dataframe)
+        
         total = len(self.dataframe)
         print(f"dataset len: {len(self.dataframe)}")
 
@@ -232,6 +233,89 @@ class PretrainDataset(Dataset):
         else:
             raise ValueError(f"Unknown pad mode {self.pad_mode}")
 
+
+    def maybe_filter_out_long_prompts(self, dataframe: datasets.Dataset = None):
+        """copy from rl_dataset.py"""
+        # filter out too long prompts
+        if self.filter_overlong_prompts:
+            tokenizer = self.tokenizer
+            processor = self.processor
+            prompt_key = self.prompt_key
+            image_key = self.image_key
+            video_key = self.video_key
+
+            if processor is not None:
+                from verl.utils.dataset.vision_utils import process_image, process_video
+
+                def doc2len(doc) -> int:
+                    try:
+                        messages = self._build_messages(doc)
+                        # pass tool schemas if available so the processor can format prompts
+                        apply_kwargs = dict(**self.apply_chat_template_kwargs)
+                        if self.tool_schemas is not None:
+                            apply_kwargs["tools"] = self.tool_schemas
+
+                        raw_prompt = self.processor.apply_chat_template(
+                            messages, add_generation_prompt=True, tokenize=False, **apply_kwargs
+                        )
+                        if image_key in doc and doc[image_key]:
+                            images = [
+                                process_image(image, image_patch_size=self.image_patch_size) for image in doc[image_key]
+                            ]
+                        else:
+                            images = None
+
+                        if video_key in doc and doc[video_key]:
+                            videos, video_metadata = zip(
+                                *[
+                                    process_video(
+                                        video, image_patch_size=self.image_patch_size, return_video_metadata=True
+                                    )
+                                    for video in doc[video_key]
+                                ],
+                                strict=True,
+                            )
+                            videos = list(videos)
+                            video_metadata = list(video_metadata)
+                            videos_kwargs = {"video_metadata": video_metadata, "do_sample_frames": False}
+                        else:
+                            videos = None
+                            videos_kwargs = {}
+
+                        return len(
+                            processor(text=[raw_prompt], images=images, videos=videos, videos_kwargs=videos_kwargs)[
+                                "input_ids"
+                            ][0]
+                        )
+                    except Exception:
+                        print("Error processing one of the samples, skipping...")
+                        traceback.print_exc()
+                        return self.max_prompt_length + 1
+
+            else:
+
+                def doc2len(doc) -> int:
+                    try:
+                        apply_kwargs = dict(**self.apply_chat_template_kwargs)
+                        if self.tool_schemas is not None:
+                            apply_kwargs["tools"] = self.tool_schemas
+
+                        return len(
+                            tokenizer.apply_chat_template(doc[prompt_key], add_generation_prompt=True, **apply_kwargs)
+                        )
+                    except Exception:
+                        print("Error processing one of the samples, skipping...")
+                        traceback.print_exc()
+                        return self.max_prompt_length + 1
+
+            dataframe = dataframe.filter(
+                lambda doc: doc2len(doc) <= self.max_prompt_length,
+                num_proc=self.num_workers,
+                desc=f"Filtering prompts longer than {self.max_prompt_length} tokens",
+            )
+
+            print(f"filter dataset len: {len(dataframe)}")
+        return dataframe
 
 class RowGroupLRUCache:
     def __init__(self, capacity: int = 4):
